df4029d3834ca0d6d48d28a4e6a22c117d8c777a
swapnil-shah <swapnilshah.me@gmail.com>
2021-01-12 13:05:09 -0500

    added dots to reflect slides

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ const slides = Array.from(track.children)
 const prevButton = document.querySelector('.carousel__button--left')
 const nextButton = document.querySelector('.carousel__button--right')
 const dotsNav = document.querySelector('.carousel__nav')
-const dotsNavIndicators = Array.from(dotsNav.children)
+const dots = Array.from(dotsNav.children)
 
 
 //Arrange the slides next to each other
@@ -27,15 +27,38 @@ slides.forEach((slide, index) => {
 nextButton.addEventListener('click', e => {
   const currentSlide = track.querySelector('.current-slide')
   const nextSlide = currentSlide.nextElementSibling
+  const currentDot = dotsNav.querySelector('.current-slide')
+  const nextDot = currentDot.nextElementSibling;
   //Move the slide
   moveToNextSlide(track, currentSlide, nextSlide)
+  updateDots(currentDot, nextDot)
+
 })
 
 //When i click left, moves slide to left
 prevButton.addEventListener('click', e => {
   const currentSlide = track.querySelector('.current-slide')
   const prevSlide = currentSlide.previousElementSibling
+  const currentDot = dotsNav.querySelector('.current-slide')
+  const prevDot = currentDot.previousElementSibling;
   moveToNextSlide(track, currentSlide, prevSlide)
+  updateDots(currentDot, prevDot)
+})
+
+//When i click nav indicators, move to that slide
+dotsNav.addEventListener('click', e => {
+  //What indicator was clicked on
+  const targetDot = e.target.closest('button')//this will prevent adding the click on the container. Will only target circles
+  if (!targetDot) return;
+  const currentSlide = track.querySelector('.current-slide')
+  const currentDot = dotsNav.querySelector('.current-slide')
+  const targetIndex = dots.findIndex(dot => dot === targetDot)
+  const targetSlide = slides[targetIndex]
+
+  moveToNextSlide(track, currentSlide, targetSlide)
+
+  updateDots(currentDot, targetDot)
+
 })
 
 function moveToNextSlide(track, currentSlide, targetSlide) {
@@ -43,4 +66,9 @@ function moveToNextSlide(track, currentSlide, targetSlide) {
   track.style.transform = `translateX(-${amountToMove})`
   currentSlide.classList.remove('current-slide')
   targetSlide.classList.add('current-slide')
+}
+
+function updateDots(currentDot, targetDot) {
+  currentDot.classList.remove('current-slide')
+  targetDot.classList.add('current-slide')
 }
